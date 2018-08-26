95a5eabc171d3faf60ae2cf496ac7d268990959f
Philip Putnam <putnamp989@gmail.com>
2018-08-26 16:32:45 -0700

    create resources, templates directories, handlebar template, render template in App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,28 +1,24 @@
 import models.User;
 import static spark.Spark.*;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
 
 public class App {
     public static void main(String[] args) {
+        staticFileLocation("/public");
+
         String[] employeeRolls = {"Happy", "Adventurer"};
         User employee = new User("Phil", employeeRolls);
-
 
         System.out.println( employee.getName() );
         String[] userRoles = employee.getRoles();
 
         for (String role : userRoles ) System.out.println(role);
 
-        get("/", (request, response) ->
-            "<!DOCTYPE html>" +
-                    "<html>" +
-                    "<head>" +
-                    "<title>Hello Friend!</title>" +
-                    "<link rel=stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css' integrity='sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO' crossorigin='anonymous'>" +
-                    "</head>" +
-                    "<body>" +
-                        "<h1>Hello to you!</h1>" +
-                    "</body>" +
-                    "</html>"
-        );
+        get("/", (request, response) -> {
+            return new ModelAndView(new HashMap(), "hello.hbs");
+                }, new HandlebarsTemplateEngine());
     }
 }
