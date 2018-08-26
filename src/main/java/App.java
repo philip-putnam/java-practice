9440c742e45c0f5e4c9e525a07753ae0b9f5069f
Philip Putnam <putnamp989@gmail.com>
2018-08-26 16:17:15 -0700

    clean up App.java, delete commented-out code

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,16 +5,12 @@ public class App {
     public static void main(String[] args) {
         String[] employeeRolls = {"Happy", "Adventurer"};
         User employee = new User("Phil", employeeRolls);
-        /* employee.name = "Phil";
-        String[] newRoles = { "Birthright", "Help Desk Specialist" };
-        System.out.println(newRoles[0]);
-         employee.setRoles(newRoles); */
+
 
         System.out.println( employee.getName() );
         String[] userRoles = employee.getRoles();
 
         for (String role : userRoles ) System.out.println(role);
-        // System.out.println( employee.roles[0] );
 
         get("/", (request, response) ->
             "<!DOCTYPE html>" +
