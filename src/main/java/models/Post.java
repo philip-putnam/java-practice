45599140b2c382dc8a03edd787a99804f4fff437
Philip Putnam <putnamp989@gmail.com>
2018-09-03 13:56:40 -0700

    import LocalDateTime, create createdAt with constructor in Post.java

--- src/main/java/models/Post.java
+++ src/main/java/models/Post.java
@@ -1,16 +1,19 @@
 package models;
 
 import java.util.ArrayList;
+import java.time.LocalDateTime;
 
 public class Post {
     private String content;
     private static ArrayList<Post> instances = new ArrayList<>();
     private boolean published;
     private int id;
+    private LocalDateTime createdAt;
 
     public Post(String content) {
         this.content = content;
         this.published = false;
+        this.createdAt = LocalDateTime.now();
         instances.add(this);
         this.id = instances.size();
     }
